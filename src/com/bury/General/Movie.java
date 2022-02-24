95d045132fb8b5505edcc44dfae9ff5a314582cc
Paulina Bury <burabury@MacBook-Pro-Paulina.local>
2022-02-24 15:38:27 +0100

    init 2

---
+++ src/com/bury/General/Movie.java
@@ -0,0 +1,22 @@
+package com.bury.General;
+
+public class Movie {
+    public static final int CHILDREN = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String title, int priceCode){
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+    public String getTitle(){
+        return _title;
+    }
+}
---
+++ src/com/bury/Main.java
@@ -0,0 +1,8 @@
+package com.bury;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
